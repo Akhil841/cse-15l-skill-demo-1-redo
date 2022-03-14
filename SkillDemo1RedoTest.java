640de738b4c53041ff786ff8ecdd933e0128f7b1
Akhil V Pillai <cs15lwi22aul@ieng6-203.ucsd.edu>
2022-03-13 17:52:52 -0700

    Fixed failing test

--- SkillDemo1RedoTest.java
+++ SkillDemo1RedoTest.java
@@ -3,6 +3,6 @@ import static org.junit.Assert.*;
 public class SkillDemo1RedoTest {
     @Test
     public void testSkillDemo1Redo() {
-        assertEquals(8, SkillDemo1Redo.returns7());
+        assertEquals(7, SkillDemo1Redo.returns7());
     }
 }
